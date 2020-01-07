667a8668608f121098c828b7428eca5ccef90061
George Hilley <gehilley@gmail.com>
2020-01-07 08:47:47 -0800

    Update weathering_model.py

--- weathering_model.py
+++ weathering_model.py
@@ -1,4 +1,59 @@
+def run_weathering_model(Lstar, Xostar, vstar, Yostar, tstar, dxstar = 0.05, r = 1/4):
+    from utils import pack_values
+    from muscl import muscl, vanAlbada
+    import numpy as np
 
+    nxstar = np.round(Lstar / dxstar)
+    x0 = np.zeros((nxstar,2))
+    x0[:,0] = Xostar
+
+
+    def bc_function(x):
+        yb = x[-1,1]
+        return (np.array([[Xostar, 1],[0, 0]]), np.array([[Xostar, yb],[0, 0]]))
+
+    def flux_function(x):
+
+        v = np.zeros_like(x)
+        v[:,1] = vstar
+        return x * v
+
+    def source_function(x):
+        X = x[:,0]
+        Y = x[:,1]
+        s = np.zeros_like(x)
+        s[:,0] = -np.power(Y,r)*np.power(X,2)
+        s[:,1] = -r*np.power(X,2)*np.power(Y,r)/Yostar
+        return s
+
+    def diffusion_function(x):
+        x_d = np.zeros((x0.shape[0]+2,2))
+        x_d[1:-1,:] = x
+        (topBC, bottomBC) = bc_function(x)
+        x_d[0,1] = topBC[0,1]
+        x_d[-1,1] = bottomBC[0,1]
+        q = np.diff(x_d[:,1]) / dxstar
+        dxdt = np.zeros_like(x)
+        dxdt[:,1] = vstar*np.diff(q) / dxstar
+        return dxdt
+
+    def prop_speed(x):
+
+        v = np.zeros_like(x)
+        v[:,1] = vstar
+        return np.abs(v)
+
+    def to_integrate(t, x):
+        x_unpacked = pack_values(x, packing_geometry=x0.shape)
+        dxdt_flux = muscl(x_unpacked, dxstar, bc_function, flux_function, vanAlbada, prop_speed = prop_speed, reconstruction='parabolic')
+        dxdt_source = source_function(x_unpacked)
+        dxdt_diffusion = diffusion_function(x_unpacked)
+        dxdt = dxdt_flux + dxdt_source + dxdt_diffusion
+        return pack_values(dxdt, packing_geometry=None)
+
+    from scipy.integrate import solve_ivp
+    out = solve_ivp(to_integrate, (np.min(tstar), np.max(tstar)), pack_values(x0, packing_geometry=None), method='LSODA', t_eval=tstar)
+    y = out.y.T
 
 def test_weathering_model():
     from utils import pack_values
