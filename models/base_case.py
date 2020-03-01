84da897bec27a052e002b0e5cfaaea45471cf47a
George Hilley <gehilley@gmail.com>
2020-03-01 15:12:12 -0800

    Update base_case.py

--- models/base_case.py
+++ models/base_case.py
@@ -11,7 +11,7 @@ X0_star = 5.27
 Y0_star = 0.000342
 v_star = 2.07
 nx = 101
-t_star = np.array([0, 4.4, 8.8, 13.2, 17.6, 22, 26.4, 30.8, 35.2, 39.6, 44]) / 10.0
+t_star = np.array([0, 4.4, 8.8, 13.2, 17.6, 22, 26.4, 30.8, 35.2, 39.6, 44]) / 1000.0
 dx_star = L_star / float(nx)
 
 x, X, Y = run_weathering_model(L_star, X0_star, v_star, Y0_star, t_star, dxstar=dx_star)
