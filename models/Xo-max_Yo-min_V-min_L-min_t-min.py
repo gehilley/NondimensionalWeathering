3d8d5dbfc0fc9f09ea5eeed89225f05390be8a6c
bgood48 <59605936+bgood48@users.noreply.github.com>
2020-03-04 16:38:45 +0100

    Add files via upload

---
+++ models/Xo-max_Yo-min_V-min_L-min_t-min.py
@@ -0,0 +1,18 @@
+filename = 'models/Xo-max_Yo-min_V-min_L-min_t-min.p'
+
+from weathering_model.weathering_model import run_weathering_model
+import numpy as np
+import pickle as p
+
+# Run model:
+L_star = 0.0381
+X0_star = 10.4
+Y0_star = 0.000171
+v_star = 0.255
+nx = 101
+t_star = np.array([0, 0.000017, 0.000035, 0.000052, 0.000070, 0.000087, 0.000104, 0.000122, 0.000139, 0.000157, 0.000174])
+dx_star = L_star / float(nx)
+
+x, X, Y = run_weathering_model(L_star, X0_star, v_star, Y0_star, t_star, dxstar=dx_star)
+
+p.dump((x, X, Y, L_star, X0_star, Y0_star, v_star, nx, t_star, dx_star), open(filename, 'wb'))
---
+++ models/Xo-min_Yo-min_V-max_L-min_t-max.py
@@ -0,0 +1,18 @@
+filename = 'models/Xo-min_Yo-min_V-max_L-min_t-max.p'
+
+from weathering_model.weathering_model import run_weathering_model
+import numpy as np
+import pickle as p
+
+# Run model:
+L_star = 0.0381
+X0_star = 0.122
+Y0_star = 0.000171
+v_star = 1040000000
+nx = 101
+t_star = np.array([0, 923, 1846, 2769, 3692, 4615, 5538, 6461, 7384, 8307, 9230])
+dx_star = L_star / float(nx)
+
+x, X, Y = run_weathering_model(L_star, X0_star, v_star, Y0_star, t_star, dxstar=dx_star)
+
+p.dump((x, X, Y, L_star, X0_star, Y0_star, v_star, nx, t_star, dx_star), open(filename, 'wb'))
