1eb2d646750e39730fb295fc129bb9a4d7e28183
bgood48 <brad_goodfellow@hotmail.com>
2020-03-26 22:46:59 +0100

    10 new cases
    10 new cases, same rationale as max/min end members but starting with base case and varying by a factor of 2.  New mins + 0.5 x base case, new maxs = 2 x base case

---
+++ models/Xo-0.5xBC_Yo-2xBC_V-0.5xBC_L-2xBC_t-2xBC.py
@@ -0,0 +1,18 @@
+filename = 'models/Xo-0.5xBC_Yo-2xBC_V-0.5xBC_L-2xBC_t-2xBC.p'
+
+from weathering_model.weathering_model import run_weathering_model
+import numpy as np
+import pickle as p
+
+# Run model:
+L_star = 15.2
+X0_star = 2.63
+Y0_star = 0.000685
+v_star = 340
+nx = 101
+t_star = np.array([0, 3.89, 7.78, 11.67, 15.56, 19.45, 23.34, 27.23, 31.12, 35.01, 38.90])
+dx_star = L_star / float(nx)
+
+x, X, Y = run_weathering_model(L_star, X0_star, v_star, Y0_star, t_star, dxstar=dx_star)
+
+p.dump((x, X, Y, L_star, X0_star, Y0_star, v_star, nx, t_star, dx_star), open(filename, 'wb'))
---
+++ models/Xo-0.5xBC_Yo-min_V-0.5xBC_L-2xBC_t-0.5xBC.py
@@ -0,0 +1,18 @@
+filename = 'models/Xo-0.5xBC_Yo-min_V-0.5xBC_L-2xBC_t-0.5xBC.p'
+
+from weathering_model.weathering_model import run_weathering_model
+import numpy as np
+import pickle as p
+
+# Run model:
+L_star = 15.2
+X0_star = 2.63
+Y0_star = 0.000171
+v_star = 340
+nx = 101
+t_star = np.array([0, 0.97, 1.94, 2.92, 3.89, 4.86, 5.83, 6.80, 7.78, 8.75, 9.72])
+dx_star = L_star / float(nx)
+
+x, X, Y = run_weathering_model(L_star, X0_star, v_star, Y0_star, t_star, dxstar=dx_star)
+
+p.dump((x, X, Y, L_star, X0_star, Y0_star, v_star, nx, t_star, dx_star), open(filename, 'wb'))
