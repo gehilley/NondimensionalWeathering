6a31e0db9a61da4b93dcc1e4f7586a946a54ac8e
bgood48 <brad_goodfellow@hotmail.com>
2020-05-15 16:57:39 +0200

    18 new models
    9 using 0.5xBC Xo and 9 using 2xBC Xo. In both sets of 9 models, Yo and Vo are 0.5BC, BC, and 2x BC. L is 2xBC and t is 2xBC, but t_step is 0.5xBC

---
+++ models/Xo-max_Yo-2xBC_V-2xBC_L-2xBC_t-2xBC_tstep-0.5xBC.py
@@ -0,0 +1,18 @@
+filename = 'models/Xo-max_Yo-2xBC_V-2xBC_L-2xBC_t-2xBC_tstep-0.5xBC.p'
+
+from weathering_model.weathering_model import run_weathering_model
+import numpy as np
+import pickle as p
+
+# Run model:
+L_star = 15.2
+X0_star = 10.4
+Y0_star = 0.000685
+v_star = 1360
+nx = 101
+t_star = np.array([0, 0.97, 1.94, 2.92, 3.89, 4.86, 5.83, 6.80, 7.78, 8.75, 9.72, 10.69, 11.66, 12.64, 13.61, 14.58, 15.55, 16.52, 17.50, 18.47, 19.44, 20.41, 21.38, 22.36, 23.33, 24.30, 25.27, 26.24, 27.22, 28.19, 29.16, 30.13, 31.10, 32.08, 33.05, 34.02, 34.99, 35.96, 36.94, 37.91, 38.88])
+dx_star = L_star / float(nx)
+
+x, X, Y = run_weathering_model(L_star, X0_star, v_star, Y0_star, t_star, dxstar=dx_star)
+
+p.dump((x, X, Y, L_star, X0_star, Y0_star, v_star, nx, t_star, dx_star), open(filename, 'wb'))
